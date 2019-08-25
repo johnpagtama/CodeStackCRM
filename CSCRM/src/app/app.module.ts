558e5839508efe9cc32e683b398cd3e7c2d9a12f
Francisco Cruz <fcruz@codestack.co>
2019-08-25 12:58:20 -0700

    login complete

--- CSCRM/src/app/app.module.ts
+++ CSCRM/src/app/app.module.ts
@@ -1,28 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MaterialModule } from './material/material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MaterialModule } from "./material/material.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-import { SidenavComponent } from './views/shared/sidenav/sidenav.component';
-import { LoginComponent } from './views/shared/login/login.component';
-import { DashboardComponent } from './views/admin/dashboard/dashboard.component';
-import { UsersComponent } from './views/admin/users/users.component';
-import { StudentsComponent } from './views/admin/students/students.component';
-import { EventsComponent } from './views/admin/events/events.component';
-import { AcademyComponent } from './views/admin/academy/academy.component';
-import { StudentInfoComponent } from './views/student/student-info/student-info.component';
-import { InstructorInfoComponent } from './views/student/instructor-info/instructor-info.component';
-import { AddEventModalComponent } from './views/shared/add-event-modal/add-event-modal.component';
-import { EventInfoComponent } from './views/admin/event-info/event-info.component';
+import { SidenavComponent } from "./views/shared/sidenav/sidenav.component";
+import { LoginComponent } from "./views/shared/login/login.component";
+import { DashboardComponent } from "./views/admin/dashboard/dashboard.component";
+import { UsersComponent } from "./views/admin/users/users.component";
+import { StudentsComponent } from "./views/admin/students/students.component";
+import { EventsComponent } from "./views/admin/events/events.component";
+import { AcademyComponent } from "./views/admin/academy/academy.component";
+import { StudentInfoComponent } from "./views/student/student-info/student-info.component";
+import { InstructorInfoComponent } from "./views/student/instructor-info/instructor-info.component";
+import { AddEventModalComponent } from "./views/shared/add-event-modal/add-event-modal.component";
+import { EventInfoComponent } from "./views/admin/event-info/event-info.component";
 
 @NgModule({
   declarations: [
@@ -54,4 +54,4 @@ import { EventInfoComponent } from './views/admin/event-info/event-info.componen
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
