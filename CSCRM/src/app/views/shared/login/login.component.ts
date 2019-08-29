4c7a12822e3672fd1b6dc5c1a781c05b3748f9f1
johnpagtama <pagtamajr@gmail.com>
2019-08-28 22:25:31 -0700

    update

--- CSCRM/src/app/views/shared/login/login.component.ts
+++ CSCRM/src/app/views/shared/login/login.component.ts
@@ -2,13 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { empty } from 'rxjs';
-<<<<<<< HEAD
 import { HttpHeaders, HttpClient } from '@angular/common/http';
-=======
-import { MatDialog } from '@angular/material/dialog';
-import { ResetPasswordModalComponent } from '../reset-password-modal/reset-password-modal.component';
-
->>>>>>> 0762982c4af1476fc2dd8bfea369842816db619f
 
 
 @Component({
@@ -17,28 +11,17 @@ import { ResetPasswordModalComponent } from '../reset-password-modal/reset-passw
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-<<<<<<< HEAD
     loginForm: FormGroup;
     userLoggedIn: boolean;
-=======
-  loginForm: FormGroup;
-  loginEmail: string;
-  password: string;
-  email: string;
->>>>>>> 0762982c4af1476fc2dd8bfea369842816db619f
 
     constructor( private builder: FormBuilder, private http: HttpClient, private router: Router) { }
 
-<<<<<<< HEAD
     ngOnInit() {
         this.loginForm = this.builder.group({
         email: ['', Validators.required],
         password: ['', Validators.required]
         });
     }
-=======
-  constructor( private builder: FormBuilder, private router: Router, private dialog: MatDialog ) { }
->>>>>>> 0762982c4af1476fc2dd8bfea369842816db619f
 
 
     async login() {
@@ -46,7 +29,6 @@ export class LoginComponent implements OnInit {
 
         const loginURL = 'https://localhost:44321/api/auth/login';
 
-<<<<<<< HEAD
         const result: any = await this.http.post
         (
             loginURL, this.loginForm.value,
@@ -72,12 +54,4 @@ export class LoginComponent implements OnInit {
     resetPassword() {
         alert('Reset Password');
     }
-=======
-  resetPassword(): void {
-    const dialogRef = this.dialog.open(ResetPasswordModalComponent, {
-      width: '350px',
-      data: {password: this.password, email: this.email}
-    });
-  }
->>>>>>> 0762982c4af1476fc2dd8bfea369842816db619f
 }
